3a5ff8d1ce202d721199741af1047271dcc57b55
Anthony-Thuita <anthony.thuita@strathmore.edu>
2020-05-26 09:55:31 +0300

    my Counties work

--- county_mix.php
+++ county_mix.php
@@ -56,5 +56,101 @@ $county_data = "1. Mombasa. – Hassan Ali Joho – ODM,
 */
 
 // @TODO : Your code starts here
-
+$countyData = explode(",",$county_data);
+$arrlength = count($countyData);
+$removeVariables = array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9",".","-");
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>County Mix</title>
+  <style>
+  table {
+       border-collapse: collapse;
+     }
+     table, th, td {
+       border: 1px solid grey;
+     }
+     table th, table td {
+       padding: 8px;
+     }
+  </style>
+</head>
+
+<body>
+        <table>
+          <thead>
+            <tr>
+              <th># </th>
+              <th>Name</th>
+              <th>Governor</th>
+
+              <th>Website</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+            $j=1;
+            for($x = 0; $x < $arrlength; $x++) {
+              $getCounty = explode(" – ",$countyData[$x]);
+              $datalength = count($getCounty);
+             ?>
+            <tr>
+              <td>
+                <?php
+                  echo $j++
+                ?>
+              </td>
+              <td>
+                <?php
+                  $Counties = str_replace($removeVariables," ", $getCounty[0]);
+                  echo $Counties;
+                  ?>
+              </td>
+              <td>
+                <?php
+                    echo $getCounty[1];
+                ?>
+
+
+                <?php
+              if(isset($getCounty[2])){
+                if($getCounty[2] == "WDM K."){
+                    echo "
+                    <img width=5% src=images/wiper.jpg style=max-width:100%>
+                        ";
+                }else{
+                  echo $getCounty[2];
+                     }
+                }
+              else{
+                if(stripos($getCounty[1]," Jubilee")){
+                  echo "
+                  <img width=5% src=images/Jubilee.jpg style=max-width:100%>
+                  ";
+                }
+                elseif(stripos($getCounty[1]," ODM")){
+                    echo "
+                    <img width=5% src=images/odm.png style=max-width:100%>
+                    ";
+                }
+              }
+              ?>
+            </td>
+              <?php
+                $Counties = str_replace($removeVariables, "", $getCounty[0]);
+                $link = strtolower(str_replace(' ', '', $Counties));?>
+              <td><a target="_BLANK" href="http://<?php echo $link?>.go.ke">http://<?php echo $link;?>.go.ke</a></td>
+            </tr>
+            <?php
+              }
+            ?>
+          </tbody>
+
+        </table>
+</body>
+
+</html>
